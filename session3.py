9a7541b2f010bf612ff523ea1497f0942d77645c
OnlyRicey <101291037+OnlyRicey@users.noreply.github.com>
2026-01-25 23:45:07 -0800

    Add files via upload

---
+++ session3.py
@@ -0,0 +1,14 @@
+def f(x):
+    
+    return x**3 + 8
+
+def main():
+    x_val = 9
+    result = f(x_val)
+    print(result)
+
+    if result > 27:
+        print("YAY!")
+
+if __name__ == "__main__":
+    main()
